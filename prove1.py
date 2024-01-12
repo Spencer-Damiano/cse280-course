ba47355fe0e31a6e7f7b72b63953e4116d95e50c
Spencer Damiano <86030230+Spencer-Damiano@users.noreply.github.com>
2024-01-12 16:16:24 -0700

    done with prove 1

---
+++ prove1.py
@@ -0,0 +1,21 @@
+def xor(p, q):
+    if p and q:
+        return False
+    if p == False and q == False:
+        return False
+
+    return True
+
+
+def truth_table_2_vars(function):
+    print(f"{function.__name__}")
+    print(f"{'p':<8}{'q':<8}{'ANS':<8}")
+    print("----------------------------------------")
+    rows = [(p,q) for p in (True, False) 
+                  for q in (True, False)]    
+    for p, q in rows:
+        ans = function(p,q)
+        print(f"{p!s:<8}{q!s:<8}{ans!s:<8}")
+    print()
+
+truth_table_2_vars(xor)
